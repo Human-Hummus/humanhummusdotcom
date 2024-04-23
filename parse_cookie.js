6ee3c498dfd399d55996758824d9dc64c6d17f7d
HumanHummus <michael.a.deross@gmail.com>
2024-04-23 14:39:52 -0700

    boykisser button

--- parse_cookie.js
+++ parse_cookie.js
@@ -1,33 +1,40 @@
 console.log(document.cookie)
 var is_uwu = false;
-if (!(document.cookie == "" || document.cookie == undefined)){
-	for (line in document.cookie.split(";")){
-		if (line.includes("enable_uwu_mode")){
-			is_uwu = true;
-			if (line.includes("false")){is_uwu=false}
-		}
-	}
-}
+if (get_cookie("uwu_mode") == 'true'){is_uwu=true}     
 //document.cookie = "yummy_cookie=choco";
 //document.cookie = "tasty_cookie=strawberry";
 console.log(document.cookie);
-if (is_uwu){
-	console.log("uwu mode engaged")
+if (is_uwu == true){
+        console.log("uwu mode engaged")
 }
 else{
-	console.log("uwu mode disengaged")
+        console.log("uwu mode disengaged")
 }
 
 function enable_uwu(){
-	if (!is_uwu){
-		is_uwu = true
-		document.cookie = "enable_uwu_mode"
-	}
+        set_cookie("uwu_mode", true)
 }
 function disable_uwu(){
-	if (is_uwu){
-		is_uwu = false
-		document.cookie = "enable_uwu_mode"
-	}
+        set_cookie("uwu_mode", false)
 }
-
+function get_cookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function set_cookie(cname, cvalue, exdays=100) { 
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
